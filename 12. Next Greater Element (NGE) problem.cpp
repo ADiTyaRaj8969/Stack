b77ad0b97295c4be2f53b8105cb077884618bf2c
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2026-01-05 22:25:38 +0530

    Add files via upload

---
+++ 12. Next Greater Element (NGE) problem.cpp
@@ -0,0 +1,34 @@
+/*
+ Task: Next Greater Element (NGE) problem
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nextGreater(const vector<int>& a)
+{
+    int n = a.size();
+    vector<int> res(n, -1);
+    stack<int> st;
+    for (int i = 0; i < n; ++i)
+    {
+        while (!st.empty() && a[i] > a[st.top()])
+        {
+            res[st.top()] = a[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return res;
+}
+
+int main()
+{
+    vector<int> a = {4, 5, 2, 25};
+    for (auto x : nextGreater(a)) cout << x << " ";
+    cout << "\n";
+    return 0;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
